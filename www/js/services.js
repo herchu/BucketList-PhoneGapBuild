9f56cf6af435a45b835cc00b1d6161b239279431
Hernan Badenes <hbadenes@ar.ibm.com>
2014-07-15 20:48:45 -0300

    service url

--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,6 @@
 angular.module('bucketList.services', [])
     .factory('API', function ($rootScope, $http, $ionicLoading, $window) {
-       var base = "http://bucketlistapplication.herokuapp.com";
+       var base = "http://topcoder-hybrid.mybluemix.com";
         $rootScope.show = function (text) {
             $rootScope.loading = $ionicLoading.show({
                 content: text ? text : 'Loading',
